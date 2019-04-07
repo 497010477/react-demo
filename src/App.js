b1c75d805d0f82bbcb443e634e089d560069e56e
冯行洲 <xingzhou.feng@hand-china.com>
2019-04-07 21:16:23 +0800

    test

--- src/App.js
+++ src/App.js
@@ -1,12 +1,50 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import LikeButton from './LikeButton';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state= {
+      condionRender: true,
+      listRender: [
+        {
+          text: "列表元素1",
+          value: "node1",
+          key: 1
+        },
+        {
+          text: "列表元素2",
+          value: "node2",
+          key: 2
+        },
+      ],
+      inputValue: '',
+      isLiked: false
+    }
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleBtnClick = this.handleBtnClick.bind(this);
+  }
+  handleChange(event){
+    this.setState({
+      inputValue: event.target.value
+    });
+  }
+  handleSubmit(event){
+    alert("Submit Name: " + this.state.inputValue);
+    event.preventDefault();
+  }
+  handleBtnClick(){
+    this.setState({
+      isLiked: !this.state.isLiked
+    });
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
+        {/* <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
@@ -19,7 +57,21 @@ class App extends Component {
           >
             Learn React
           </a>
-        </header>
+        </header> */}
+        {this.state.condionRender?<p>真</p>:<p>假</p>}
+        <ul>
+        {this.state.listRender.map(
+          listnode=> <li key= {listnode.key}>{listnode.text}</li>
+        )}
+        </ul>
+        <form onSubmit={this.handleSubmit}>
+        <label>
+          Name:
+          <input type="text" value={this.state.inputValue} onChange={this.handleChange} />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+      <LikeButton isLiked={this.state.isLiked} buttonClick={this.handleBtnClick} />
       </div>
     );
   }
